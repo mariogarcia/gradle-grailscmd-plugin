7b892c89efb979c90d7753ae69259a143c0f153a
Mario Garcia <mario.ggar@gmail.com>
2014-01-29 00:35:43 +0100

    Fixed compilation error

--- src/main/groovy/com/bitability/gradle/plugin/frege/FregePlugin.groovy
+++ src/main/groovy/com/bitability/gradle/plugin/frege/FregePlugin.groovy
@@ -3,11 +3,12 @@ package com.bitability.gradle.plugin.frege
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.BasePlugin
-import org.gradle.api.plugins.LanguageBasePlugin
+import org.gradle.language.base.plugins.LanguageBasePlugin
 
 class FregePlugin implements Plugin<Project> {
 
     void apply(Project project) {
+
         project.plugins.apply(BasePlugin)
         project.plugins.apply(LanguageBasePlugin)
 
